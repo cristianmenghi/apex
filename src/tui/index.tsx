077089b1ba8c70b7616cf09631ecd317e16fa070
Jorge Raad <jorge@pensarai.com>
2026-03-19 14:03:27 -0400

    feat: unify model selection as a dialog for both home and operator mode
    Model picker now opens as a Dialog overlay (like /themes) instead of navigating
    to a full-page route. Also adds evt.preventDefault() to ModelPicker's keyboard
    handler to stop keys from leaking through to underlying components.

--- src/tui/index.tsx
+++ src/tui/index.tsx
@@ -523,6 +523,9 @@ function CommandDisplay({
           <RouteSwitch.Case when="help">
             <HelpDialog />
           </RouteSwitch.Case>
+          <RouteSwitch.Case when="credits">
+            <CreditsFlow onOpenAuthDialog={onOpenAuthDialog} />
+          </RouteSwitch.Case>
           <RouteSwitch.Case when="skills">
             <SkillsDialog />
           </RouteSwitch.Case>
